4edb81d7bfdf0541b7523810a97f9abc80662776
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-16 19:48:51 +0700

    update

---
+++ basic.py
@@ -0,0 +1,15 @@
+# print out on console/terminal
+print("hello world")
+
+# string
+char_name = "John"
+char_age = '23'
+
+print(char_name + "is " + char_age + ' years old.')
+
+# number
+num1 = 123
+num2 = 0.1281
+
+# boolean
+is_male = True
---
+++ string.py
@@ -0,0 +1,8 @@
+copyright = "\n\n© Micheal"
+
+print("Hello\n\"World\"")
+print(copyright)
+
+phrase = "This iS AwESoMe"
+print(phrase.lower()) # this is awesome
+print(phrase.isupper()) # false
