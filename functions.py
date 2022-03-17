a6681acb58d7a2628c4effd99e77dc0655d76179
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-17 16:49:47 +0700

    update

---
+++ functions.py
@@ -0,0 +1,16 @@
+# you need to write the code indented
+def say_something():
+   print("What's up")
+
+print("Hi")
+say_something()
+print("Bye")
+# ^^^^^^^
+# Hi
+# Hello User!
+# Bye
+
+def sayhi_to(name):
+   print("Hello " + name + "!")
+
+sayhi_to("Mike")
