7d6c1546023e4c067214a741d3960843a2838c30
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-17 21:25:24 +0700

    update

--- tuples.py
+++ tuples.py
@@ -1,8 +1,8 @@
 # tuple is immutable unlike array (can not change)
-coordinates = (4, 5)
+coordinates = (4, 5, 6)
 
 # error
 # coordinates[1] = 15
 
-print(coordinates) # (4, 5)
+print(coordinates) # (4, 5, 6)
 print(coordinates[0]) # 4
