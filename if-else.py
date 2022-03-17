2d52038a5d01d474c304c8bb68774980ea4711af
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-17 21:17:57 +0700

    update

--- if-else.py
+++ if-else.py
@@ -3,13 +3,19 @@ age = int(age)
 
 if age < 10:
    print("Go home and study.")
-if age < 20:
+elif age < 20:
    print("You can not drink alcohol, you are under age.")
 else:
    print("What would you like to order.")
 
-def is_tall_male(is_tall, is_male):
+def tall_male(is_tall, is_male):
    if is_tall and is_male:
       print("You are a tall man")
-   if is_tall and not is_male:
+   elif is_tall and not is_male:
       print("You are a tall woman")
+   elif not is_tall and is_male:
+      print("You are a short man")
+   else:
+      print("You are a short woman")
+
+tall_male(True, False)
