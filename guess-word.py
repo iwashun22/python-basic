e8fd01a202b6cacf57a06250662bd8586434bb3d
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-18 15:50:44 +0700

    update

---
+++ guess-word.py
@@ -0,0 +1,13 @@
+word = "hello"
+guess_word = ""
+limit = 3
+count_limit = 0
+
+while guess_word != word and count_limit < limit:
+   guess_word = input("Guess the word: ")
+   count_limit += 1
+
+if guess_word == word:
+   print("You win!")
+else:
+   print("Out of guess, you lose")
