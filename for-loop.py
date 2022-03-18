b347022dde4d5c242719a04a75135987dc30333a
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-18 17:33:39 +0700

    update

---
+++ for-loop.py
@@ -0,0 +1,28 @@
+def separate():
+   print("=======================")
+
+# Loop by a string
+for letter in "Hello World!":
+   print(letter)
+separate()
+
+# Loop through array
+hobbies = ["Basketball", "Chess", "Drawing"]
+for hobby in hobbies:
+   print(hobby)
+separate()
+
+# Loop with number
+for i in range(10):
+   print(i)
+   # this will print until 9, not incluing 10
+separate()
+
+for x in range(3, 8):
+   print(x)
+   # this will start to print from 3 until 7
+separate()
+
+for i in range(len(hobbies)):
+   print("index [" + str(i) + "]: " + hobbies[i])
+separate()
