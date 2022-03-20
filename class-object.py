4eb370566ab1da266cb1e12293b2f63cff746cf0
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-20 09:09:12 +0700

    update

--- class-object.py
+++ class-object.py
@@ -6,8 +6,17 @@ class Student:
       self.age = age
       self.is_good_at_math = is_good_at_math
 
+   def on_honer_roll(self):
+      if self.gpa >= 3.5:
+         return True
+      else:
+         return False
+
 student1 = Student("Robert", 3.1, 17, True)
-student2 = Student("Emma", 3.4, 16, False)
+student2 = Student("Emma", 3.6, 16, False)
 
 print(student1.name, student1.gpa)
 print(student2.name, student2.gpa)
+
+print(student1.on_honer_roll())
+print(student2.on_honer_roll())
