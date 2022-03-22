f5d22d8f21bde006a3cecfce7b4a352782284cfb
Shun Iwashita <iwashunabc519@gmail.com>
2022-03-22 09:02:22 +0700

    update

--- string.py
+++ string.py
@@ -4,6 +4,12 @@ copyright = "\n\n© Micheal"
 print("Hello\n\"World\"")
 print(copyright)
 
+# multiple line string
+# forward slash is to not make a new line
+print("""Line 1
+line \
+2""")
+
 phrase = "This iS AwESoMe"
 print(phrase.lower()) # this is awesome
 
@@ -14,6 +20,9 @@ print(phrase.upper().isupper()) # True
 print(len(phrase)) # 15
 
 print(phrase[8]) # A
+
+# this will print the 2nd index from backward
+print(phrase[-2]) # M
 
 # find the first index that contain in the parameter
 print(phrase.index("i")) # 2 
