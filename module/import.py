3ac38a070e878c09f3ce210dc96e818a3b7dc2c7
Shun <85538338+iwashun22@users.noreply.github.com>
2023-02-25 00:52:50 +0700

    Delete module directory

--- module/import.py
+++
@@ -1,7 +0,0 @@
-import modules
-
-print(modules.feet_in_mile)
-print(modules.get_file_extension("script.js"))
-
-roll_dice = modules.get_random_int(6)
-print(roll_dice)
--- module/modules.py
+++
@@ -1,9 +0,0 @@
-import random
-
-feet_in_mile = 5280
-
-def get_file_extension(filename):
-   return filename[filename.find(".") + 1:]
-
-def get_random_int(num):
-   return random.randint(1, num)
