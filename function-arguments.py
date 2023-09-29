623dbfe2e40bdee182d1972aa7ff21bf5757e935
Shun Iwashita <iwashunabc519@gmail.com>
2023-09-29 21:05:01 +0700

    update

---
+++ function-arguments.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+def square(x):
+  """Return square of a given number
+
+  Parameters
+  ------------
+  x : int
+
+  Return -> int
+  """ # this is called docstring
+  square = x ** 2
+  return square
+
+print(square(3)) # 9
+
+# set default value
+def custom_print(txt="Hello World", suffix="!"):
+  """Print text with a suffix.
+
+  Parameters
+  ------------
+  txt : str
+        text or a message.
+  suffix : str
+        suffix such as ".,!?"
+  
+  Return -> void
+  """
+  print(txt + suffix)
+  return
+
+custom_print("hi") # expect: hi!
+custom_print("what", "?") # expect: what?
+custom_print(suffix=",") # expect: Hello World,
+
+# FIXME: when you provide the arguments without in order, you need to specify or else get an error.
+custom_print(suffix="!!", "Some Text") # error
+
+custom_print(suffix="!!", txt="Success") # expect: Success!!
